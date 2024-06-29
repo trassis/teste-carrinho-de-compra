b80dc03770ff4b464236c060024e17279e0510bd
trassis <essemeia@gmail.com>
2024-06-29 19:24:01 -0300

    Testes finalizados

--- TesteShoppingCart.java
+++ TesteShoppingCart.java
@@ -15,31 +15,60 @@ import java.util.List;
 public class TesteShoppingCart {
 
   private ShoppingCart shoppingCart;
+  private Item item1, item2;
+  private double EPSILON;
 
   @Before
   public void setUp() {
     shoppingCart = new ShoppingCart();
-    shoppingCart.addItem(new Item("ESM", 65.0));
-    shoppingCart.addItem(new Item("GoF", 120.0));
+
+    item1 = new Item("Item1", 10.0);    
+    item2 = new Item("Item2", 20.0);    
+
+    EPSILON = 1e-5;
   }
 
   @Test
   public void testAddItem() {
-    // Escreva aqui seu código de teste
+    shoppingCart.addItem(item1);
+    shoppingCart.addItem(item2);
+
+    Item cartItem1 = shoppingCart.getItems().get(0);
+    Item cartItem2 = shoppingCart.getItems().get(1);
+
+    assertEquals(item1.getName(), cartItem1.getName());
+    assertEquals(item1.getPrice(), cartItem1.getPrice(), EPSILON);
+
+    assertEquals(item2.getName(), cartItem2.getName());
+    assertEquals(item2.getPrice(), cartItem2.getPrice(), EPSILON);
   }
 
   @Test
   public void testRemoveItem() {
-    // Escreva aqui seu código de teste
+    shoppingCart.addItem(item1);
+    shoppingCart.addItem(item2);
+
+    shoppingCart.removeItem(item1);
+    assertEquals(shoppingCart.getItemCount(), 1);
+    shoppingCart.removeItem(item2);
+    assertEquals(shoppingCart.getItemCount(), 0);
   }
 
   @Test
   public void testGetTotalPrice() {
-    // Escreva aqui seu código de teste
+    shoppingCart.addItem(item1);
+    assertEquals(shoppingCart.getTotalPrice(), 10.0, EPSILON);
+
+    shoppingCart.addItem(item2);
+    assertEquals(shoppingCart.getTotalPrice(), 30.0, EPSILON);
   }
 
   @Test
   public void testClearCart() {
-    // Escreva aqui seu código de teste
+    shoppingCart.addItem(item1);
+    shoppingCart.addItem(item2);
+
+    shoppingCart.clearCart();
+    assertEquals(shoppingCart.getItemCount(), 0);
   }
 }
